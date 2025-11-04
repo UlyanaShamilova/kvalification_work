958997ea94456d3d6f3f8227bd962e3594233a60
Ulyana <ulanasamileva1@gmail.com>
2025-11-05 00:31:17 +0200

    correct login/register page

--- Models/LogModel.cs
+++ Models/LogModel.cs
@@ -4,9 +4,11 @@ namespace project.Models
 {
     public class LogModel
     {
+        [Display(Name = "Введіть ім'я: ")]
         [Required(ErrorMessage = "Введіть ім'я")]
         public string username { get; set; }
 
+        [Display(Name = "Введіть пароль: ")]
         [Required(ErrorMessage = "Введіть пароль")]
         public string password { get; set; }
     }
--- Models/RegModel.cs
+++ Models/RegModel.cs
@@ -4,13 +4,18 @@ namespace project.Models
 {
     public class RegModel
     {
+        [Display(Name = "Введіть ім'я: ")]
         [Required(ErrorMessage = "Введіть ім'я")]
         public string username { get; set; }
 
-        [Required(ErrorMessage = "Введіть email")]
+        [Display(Name = "Введіть email: ")]
         [EmailAddress(ErrorMessage = "Неправильний email")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@gmail\.com$", ErrorMessage = "Email має містити @gmail.com")]
+        [Required(ErrorMessage = "Введіть email")]
         public string email { get; set; }
 
+        [Display(Name = "Введіть пароль: ")]
+        [MinLength(6, ErrorMessage = "Пароль має бути не менше 6 символів")]
         [Required(ErrorMessage = "Введіть пароль")]
         public string password { get; set; }
     }
